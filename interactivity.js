3ab4c0b49ff791cc40578c9fe6ff00d14ba1dc2d
ViorelOncea <142790865+ViorelOncea@users.noreply.github.com>
2024-03-20 15:52:34 +0200

    First commit - Portofolio Project

---
+++ interactivity.js
@@ -0,0 +1,193 @@
+// Image 1
+let oneFull = document.getElementById('onefull');
+let oneText = document.getElementById('one');
+let oneimage = document.getElementById('imageone');
+
+oneFull.addEventListener('mouseenter', function() {
+  oneText.style.display = 'none';
+  oneimage.style.display = 'inline-block';
+});
+oneFull.addEventListener('mouseleave', function() {
+  oneText.style.display = 'inline-block';
+  oneimage.style.display = 'none';
+});
+
+
+// Image 2
+let twoFull = document.getElementById('twofull');
+let twoText = document.getElementById('two');
+let twoimage = document.getElementById('imagetwo');
+
+twoFull.addEventListener('mouseenter', function() {
+  twoText.style.display = 'none';
+  twoimage.style.display = 'inline-block';
+});
+twoFull.addEventListener('mouseleave', function() {
+  twoText.style.display = 'inline-block';
+  twoimage.style.display = 'none';
+});
+
+
+// Image 3
+let threeFull = document.getElementById('threefull');
+let threeText = document.getElementById('three');
+let threeimage = document.getElementById('imagethree');
+
+threeFull.addEventListener('mouseenter', function() {
+  threeText.style.display = 'none';
+  threeimage.style.display = 'inline-block';
+});
+threeFull.addEventListener('mouseleave', function() {
+  threeText.style.display = 'inline-block';
+  threeimage.style.display = 'none';
+});
+
+
+// Image 4
+let fourFull = document.getElementById('fourfull');
+let fourText = document.getElementById('four');
+let fourimage = document.getElementById('imagefour');
+
+fourFull.addEventListener('mouseenter', function() {
+  fourText.style.display = 'none';
+  fourimage.style.display = 'inline-block';
+});
+fourFull.addEventListener('mouseleave', function() {
+  fourText.style.display = 'inline-block';
+  fourimage.style.display = 'none';
+});
+
+
+// Image 5
+let fiveFull = document.getElementById('fivefull');
+let fiveText = document.getElementById('five');
+let fiveimage = document.getElementById('imagefive');
+
+fiveFull.addEventListener('mouseenter', function() {
+  fiveText.style.display = 'none';
+  fiveimage.style.display = 'inline-block';
+});
+fiveFull.addEventListener('mouseleave', function() {
+  fiveText.style.display = 'inline-block';
+  fiveimage.style.display = 'none';
+});
+
+
+// Image 3 again
+let threeFullAgain = document.getElementById('threefullagain');
+let threeTextAgain = document.getElementById('threeagain');
+let threeImageAgain = document.getElementById('imagethreeagain');
+
+threeFullAgain.addEventListener('mouseenter', function() {
+  threeTextAgain.style.display = 'none';
+  threeImageAgain.style.display = 'inline-block';
+});
+threeFullAgain.addEventListener('mouseleave', function() {
+  threeTextAgain.style.display = 'inline-block';
+  threeImageAgain.style.display = 'none';
+});
+
+
+// Image 6
+let sixFull = document.getElementById('sixfull');
+let sixText = document.getElementById('six');
+let siximage = document.getElementById('imagesix');
+
+sixFull.addEventListener('mouseenter', function() {
+  sixText.style.display = 'none';
+  siximage.style.display = 'inline-block';
+});
+sixFull.addEventListener('mouseleave', function() {
+  sixText.style.display = 'inline-block';
+  siximage.style.display = 'none';
+});
+
+
+// Image 7
+let sevenFull = document.getElementById('sevenfull');
+let sevenText = document.getElementById('seven');
+let sevenimage = document.getElementById('imageseven');
+
+sevenFull.addEventListener('mouseenter', function() {
+  sevenText.style.display = 'none';
+  sevenimage.style.display = 'inline-block';
+});
+sevenFull.addEventListener('mouseleave', function() {
+  sevenText.style.display = 'inline-block';
+  sevenimage.style.display = 'none';
+});
+
+
+// Image 5 again
+let fiveFullAgain = document.getElementById('fivefullagain');
+let fiveTextAgain = document.getElementById('fiveagain');
+let fiveImageAgain = document.getElementById('imagefiveagain');
+
+fiveFullAgain.addEventListener('mouseenter', function() {
+  fiveTextAgain.style.display = 'none';
+  fiveImageAgain.style.display = 'inline-block';
+});
+fiveFullAgain.addEventListener('mouseleave', function() {
+  fiveTextAgain.style.display = 'inline-block';
+  fiveImageAgain.style.display = 'none';
+});
+
+
+// Image 2 again
+let twoFullAgain = document.getElementById('twofullagain');
+let twoTextAgain = document.getElementById('twoagain');
+let twoImageAgain = document.getElementById('imagetwoagain');
+
+twoFullAgain.addEventListener('mouseenter', function() {
+  twoTextAgain.style.display = 'none';
+  twoImageAgain.style.display = 'inline-block';
+});
+twoFullAgain.addEventListener('mouseleave', function() {
+  twoTextAgain.style.display = 'inline-block';
+  twoImageAgain.style.display = 'none';
+});
+
+
+// Image 1 again
+let oneFullAgain = document.getElementById('onefullagain');
+let oneTextAgain = document.getElementById('oneagain');
+let oneImageAgain = document.getElementById('imageoneagain');
+
+oneFullAgain.addEventListener('mouseenter', function() {
+  oneTextAgain.style.display = 'none';
+  oneImageAgain.style.display = 'inline-block';
+});
+oneFullAgain.addEventListener('mouseleave', function() {
+  oneTextAgain.style.display = 'inline-block';
+  oneImageAgain.style.display = 'none';
+});
+
+
+// Image 1 again
+let oneFullAgainX = document.getElementById('onefullagainx');
+let oneTextAgainX = document.getElementById('oneagainx');
+let oneImageAgainX = document.getElementById('imageoneagainx');
+
+oneFullAgainX.addEventListener('mouseenter', function() {
+  oneTextAgainX.style.display = 'none';
+  oneImageAgainX.style.display = 'inline-block';
+});
+oneFullAgainX.addEventListener('mouseleave', function() {
+  oneTextAgainX.style.display = 'inline-block';
+  oneImageAgainX.style.display = 'none';
+});
+
+
+// Image 8
+let eightFull = document.getElementById('eightfull');
+let eightText = document.getElementById('eight');
+let eightimage = document.getElementById('imageeight');
+
+eightFull.addEventListener('mouseenter', function() {
+  eightText.style.display = 'none';
+  eightimage.style.display = 'inline-block';
+});
+eightFull.addEventListener('mouseleave', function() {
+  eightText.style.display = 'inline-block';
+  eightimage.style.display = 'none';
+});
